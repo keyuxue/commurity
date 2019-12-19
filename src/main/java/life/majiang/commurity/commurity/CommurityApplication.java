12e059b5c553f578ed9f7d8aeb9b64a2560d60b7
Your Name <you@example.com>
2019-12-19 22:59:43 +0800

    这是一个全新的springboot社区项目,希望自己可以有能力提升.

---
+++ src/main/java/life/majiang/commurity/commurity/CommurityApplication.java
@@ -0,0 +1,13 @@
+package life.majiang.commurity.commurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommurityApplication.class, args);
+    }
+
+}
